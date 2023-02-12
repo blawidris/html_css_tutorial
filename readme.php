c0d9aac4b953d581a8650d3386d15067e9cedc2a
blawidris <blawidris@gmail.com>
2023-02-12 01:45:05 +0100

    HTML scratch course

--- readme.php
+++ readme.php
@@ -1,1 +1,15 @@
-# HTML Tutorial
+# What is HTML (HyperText Markup Language)
+- Used to structure content on a web page (image, text, form, etc)
+- Web content are structured using ***HTML tags***
+- Some ***HTML tags*** has a open/end tag while some only has a open tag
+*e.g*
+
+```
+<!-- Paragraph tag -->
+<p>Content</p>
+<!-- heading -->
+<h1>Heading 1</h1>
+
+<!-- Image  has no end tag -->
+<img src="image.png"/>
+```
