3b726ee1c93369aa4c61e0ef680c1d0dc307746b
blawidris <blawidris@gmail.com>
2023-03-09 22:39:28 +0100

    file update

--- readme.php
+++ readme.php
@@ -2,14 +2,18 @@
 - Used to structure content on a web page (image, text, form, etc)
 - Web content are structured using ***HTML tags***
 - Some ***HTML tags*** has a open/end tag while some only has a open tag
-*e.g*
+
+
+**Syntax**
 
 ```
-<!-- Paragraph tag -->
-<p>Content</p>
-<!-- heading -->
-<h1>Heading 1</h1>
-
-<!-- Image  has no end tag -->
-<img src="image.png"/>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>HTML Boilerplate</title>
+</head>
+<body>
+    HTML body
+</body>
+</html>
 ```
